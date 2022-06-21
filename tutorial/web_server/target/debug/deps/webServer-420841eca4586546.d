2c9a5921cba3a62934a6c7afa4c2b4e53d27213e
Chris Pfaff <chrispfaff10@gmail.com>
2022-06-21 11:20:48 -0500

    deleted other directories

--- tutorial/web_server/target/debug/deps/webServer-420841eca4586546.d
+++
@@ -1,5 +0,0 @@
-/Users/chrispfaff/Projects/rust_tuts/tutorial/web_server/target/debug/deps/webServer-420841eca4586546: src/main.rs
-
-/Users/chrispfaff/Projects/rust_tuts/tutorial/web_server/target/debug/deps/webServer-420841eca4586546.d: src/main.rs
-
-src/main.rs:
--- tutorial/web_server/target/debug/webServer.d
+++
@@ -1,1 +0,0 @@
-/Users/chrispfaff/Projects/rust_tuts/tutorial/web_server/target/debug/webServer: /Users/chrispfaff/Projects/rust_tuts/tutorial/web_server/src/main.rs
